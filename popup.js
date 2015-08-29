8e9675b4307480b01c574216facb57c56bf6b84a
Alex Gaynor <alex.gaynor@gmail.com>
2015-08-29 17:42:34 -0400

    remove the "http:" prefix on each thing

--- popup.js
+++ popup.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
         var html = "<table>";
         var data = sortByValue(values);
         for (var val in data.slice(0, 20)) {
-            html += "<tr><td>" + data[val][0] + "</td><td>" + data[val][1] + "</td></tr>";
+            html += "<tr><td>" + data[val][0].slice(5) + "</td><td>" + data[val][1] + "</td></tr>";
         }
         html += "</table>";
         document.getElementById("stats").innerHTML = html;
