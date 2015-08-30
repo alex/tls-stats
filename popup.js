6be73225c3bb8fba6f5550a36c6f06dae0f2ffd2
Alex Gaynor <alex.gaynor@gmail.com>
2015-08-29 20:35:03 -0400

    include percentage of total requests, clean up

--- popup.js
+++ popup.js
@@ -1,23 +1,26 @@
 function sortByValue(data) {
     var values = [];
+    var count = 0;
     for (var key in data) {
+        count += data[key];
         values.push([key, data[key]]);
     }
     values.sort(function(a, b) {
         return b[1] - a[1];
     });
-    return values;
+    return {values: values, total: count};
 }
 
 document.addEventListener("DOMContentLoaded", function() {
     chrome.storage.local.get(null, function(values) {
-        var html = "<table>";
+        var html = "";
         var data = sortByValue(values);
-        for (var val in data.slice(0, 20)) {
-            html += "<tr><td>" + data[val][0].slice(5) + "</td><td>" +
-                data[val][1] + "</td></tr>";
+        for (var value in data.values.slice(0, 20)) {
+            var val = data.values[value];
+            html += "<tr><td>" + val[0].slice(5) + "</td><td>" +
+                val[1] + "</td><td>" + Math.round(100 * val[1] / data.total) +
+                "%</td></tr>";
         }
-        html += "</table>";
-        document.getElementById("stats").innerHTML = html;
+        document.getElementById("stats-body").innerHTML = html;
     });
 });
