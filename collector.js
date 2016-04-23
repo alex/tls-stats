fdb3a132f80d938a70e93354a808401be4c50c9a
Alex Gaynor <alex.gaynor@gmail.com>
2016-04-23 08:42:47 -0400

    Fixed badge logic

--- collector.js
+++ collector.js
@@ -1,3 +1,10 @@
+chrome.tabs.onCreated.addListener(function(tab) {
+    chrome.browserAction.setBadgeText({
+        text: "",
+        tabId: tab.id
+    });
+});
+
 chrome.webRequest.onBeforeSendHeaders.addListener(
     function(info) {
         var url = new URL(info.url);
@@ -6,13 +13,15 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
             return;
         }
         if (url.protocol == "http:") {
-            chrome.browserAction.getBadgeText({tabId: info.tabId}, function(value) {
-                var newValue = (value ? parseInt(value) : 0) + 1;
-                chrome.browserAction.setBadgeText({
-                    text: newValue.toString(),
-                    tabId: info.tabId
+            if (info.tabId !== -1) {
+                chrome.browserAction.getBadgeText({tabId: info.tabId}, function(value) {
+                    var newValue = (value ? parseInt(value) : 0) + 1;
+                    chrome.browserAction.setBadgeText({
+                        text: newValue.toString(),
+                        tabId: info.tabId
+                    });
                 });
-            });
+            }
             var key = "http:" + url.hostname;
             chrome.storage.local.get(key, function(values) {
                 var data = {};
